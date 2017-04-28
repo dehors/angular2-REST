2d7e1ab76f258bc97a99f5838b674213573c7b12
slam24 <luismartinez242011@gmail.com>
2017-04-28 02:41:32 -0600

    feat: [(ngModel)]

--- src/app/code-font/code-font.component.ts
+++ src/app/code-font/code-font.component.ts
@@ -9,6 +9,7 @@ export class CodeFontComponent implements OnInit {
   title = "List of costumers";
   condition = true;
   costumers = ["Juan","Pedro","Maria","Alejandro"];
+  name = 'Juan';
   constructor() { }
 
   ngOnInit() {
